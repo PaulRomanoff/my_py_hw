321b67f762d61a402558be2aacc5a2fc0b561132
Paul <aenzoten@gmail.com>
2022-02-16 16:50:37 +0300

    task 2.1-2.2 added

---
+++ class_0-1.py
@@ -0,0 +1,30 @@
+# 2.1
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
+
+# 2.2
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#2.2*
+print("    *", "     *", sep="       ")
+print("   * *", "   * *", sep="       ")
+print("  *   *", "  *   *", sep="      ")
+print(" *     *", " *     *", sep="     ")
+print("***   ***", "***   ***", sep="    ")
+print("  *   *", "  *   *", sep="      ")
+print("  *   *", "  *   *", sep="      ")
+print("  *****", "  *****", sep="      ")
+
+#2.2**
+print("    *\n   * *\n  *   *\n *     *")
+print(3*"*", 3*"*", sep="   ")
+print("  *   *\n  *   *\n  *****")
+
+#2.3
