1649ea21cb5e9e89d0d367e67b63d270fbb4dca0
Paul <aenzoten@gmail.com>
2022-03-10 18:02:33 +0300

    WH and CW 7 done

--- HW_6/packages/board_n_check/w.py
+++
@@ -1,8 +0,0 @@
-# tt winner check
-def win_check(checklist):
-    win_b = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
-    for each in win_b:
-        if checklist[each[0]] == checklist[each[1]] == checklist[each[2]]:
-            return print(checklist[each[0]], "win")
-            break
-    return False
---
+++ program_hw_mysplit/main.py
@@ -0,0 +1,3 @@
+# manual split from packages.mysplit
+from packages.mysplit import mysplitP
+mysplitP.mysplit_func()
